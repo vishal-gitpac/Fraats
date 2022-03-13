a6fbe8472ab07c7ea5d161e6117ee3c7c500ae32
sairaj <sairaj2119@gmail.com>
2022-03-13 12:07:33 +0530

    uncomment overdies of mui styles

--- client/src/styles/customTheme.js
+++ client/src/styles/customTheme.js
@@ -15,53 +15,53 @@ const customTheme = (darkMode) =>
       },
     },
     overrides: {
-      // MuiTypography: {
-      //   root: {
-      //     wordBreak: "break-word",
-      //     color: "#FFF",
-      //   },
-      //   h6: {
-      //     color: "#604f4f",
-      //   },
-      //   h5: {
-      //     color: "#604f4f",
-      //   },
-      //   button: {
-      //     color: "#604f4f",
-      //   },
-      //   caption: {
-      //     color: "#604f4f",
-      //   },
-      //   comments: {
-      //     color: "#000000",
-      //   },
-      //   body2: {
-      //     color: "#604f4f",
-      //   },
-      //   subtitle2: {
-      //     color: "#604f4f",
-      //   },
-      // },
-      // MuiFormControl: {
-      //   root: {
-      //     flexDirection: "unset",
-      //   },
-      // },
-      // MuiAvatar: {
-      //   colorDefault: {
-      //     backgroundColor: "#45AB0F",
-      //   },
-      // },
-      // MuiButton: {
-      //   textPrimary: {
-      //     color: "#ffffff",
-      //   },
-      // },
-      // MuiIconButton: {
-      //   colorPrimary: {
-      //     color: "#ffffff",
-      //   },
-      // },
+      MuiTypography: {
+        root: {
+          wordBreak: "break-word",
+          color: "#FFF",
+        },
+        h6: {
+          color: "#604f4f",
+        },
+        h5: {
+          color: "#604f4f",
+        },
+        button: {
+          color: "#604f4f",
+        },
+        caption: {
+          color: "#604f4f",
+        },
+        comments: {
+          color: "#000000",
+        },
+        body2: {
+          color: "#604f4f",
+        },
+        subtitle2: {
+          color: "#604f4f",
+        },
+      },
+      MuiFormControl: {
+        root: {
+          flexDirection: "unset",
+        },
+      },
+      MuiAvatar: {
+        colorDefault: {
+          backgroundColor: "#45AB0F",
+        },
+      },
+      MuiButton: {
+        textPrimary: {
+          color: "#ffffff",
+        },
+      },
+      MuiIconButton: {
+        colorPrimary: {
+          color: "#ffffff",
+        },
+      },
     },
   });
 
