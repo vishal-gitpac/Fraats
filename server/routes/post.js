3b8d45a2f00da83a7b3f0242d2dd1fc9b44700cb
sairaj <sairaj2119@gmail.com>
2022-03-17 11:15:43 +0530

    fix post delete

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { auth } = require('../utils/middleware');
+const express = require("express");
+const { auth } = require("../utils/middleware");
 const {
   getPosts,
   getSubscribedPosts,
@@ -8,8 +8,8 @@ const {
   createNewPost,
   updatePost,
   deletePost,
-} = require('../controllers/post');
-const { upvotePost, downvotePost } = require('../controllers/postVote');
+} = require("../controllers/post");
+const { upvotePost, downvotePost } = require("../controllers/postVote");
 const {
   postComment,
   deleteComment,
@@ -17,43 +17,43 @@ const {
   postReply,
   deleteReply,
   updateReply,
-} = require('../controllers/postComment');
+} = require("../controllers/postComment");
 const {
   upvoteComment,
   downvoteComment,
   upvoteReply,
   downvoteReply,
-} = require('../controllers/commentVote');
+} = require("../controllers/commentVote");
 
 const router = express.Router();
 
 //CRUD posts routes
-router.get('/', getPosts);
-router.get('/search', getSearchedPosts);
-router.get('/:id/comments', getPostAndComments);
-router.get('/subscribed', auth, getSubscribedPosts);
-router.post('/', auth, createNewPost);
-router.patch('/:id', auth, updatePost);
-router.delete(':id', auth, deletePost);
+router.get("/", getPosts);
+router.get("/search", getSearchedPosts);
+router.get("/:id/comments", getPostAndComments);
+router.get("/subscribed", auth, getSubscribedPosts);
+router.post("/", auth, createNewPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
 
 //posts vote routes
-router.post('/:id/upvote', auth, upvotePost);
-router.post('/:id/downvote', auth, downvotePost);
+router.post("/:id/upvote", auth, upvotePost);
+router.post("/:id/downvote", auth, downvotePost);
 
 //post comments routes
-router.post('/:id/comment', auth, postComment);
-router.delete('/:id/comment/:commentId', auth, deleteComment);
-router.patch('/:id/comment/:commentId', auth, updateComment);
-router.post('/:id/comment/:commentId/reply', auth, postReply);
-router.delete('/:id/comment/:commentId/reply/:replyId', auth, deleteReply);
-router.patch('/:id/comment/:commentId/reply/:replyId', auth, updateReply);
+router.post("/:id/comment", auth, postComment);
+router.delete("/:id/comment/:commentId", auth, deleteComment);
+router.patch("/:id/comment/:commentId", auth, updateComment);
+router.post("/:id/comment/:commentId/reply", auth, postReply);
+router.delete("/:id/comment/:commentId/reply/:replyId", auth, deleteReply);
+router.patch("/:id/comment/:commentId/reply/:replyId", auth, updateReply);
 
 //comment vote routes
-router.post('/:id/comment/:commentId/upvote', auth, upvoteComment);
-router.post('/:id/comment/:commentId/downvote', auth, downvoteComment);
-router.post('/:id/comment/:commentId/reply/:replyId/upvote', auth, upvoteReply);
+router.post("/:id/comment/:commentId/upvote", auth, upvoteComment);
+router.post("/:id/comment/:commentId/downvote", auth, downvoteComment);
+router.post("/:id/comment/:commentId/reply/:replyId/upvote", auth, upvoteReply);
 router.post(
-  '/:id/comment/:commentId/reply/:replyId/downvote',
+  "/:id/comment/:commentId/reply/:replyId/downvote",
   auth,
   downvoteReply
 );
